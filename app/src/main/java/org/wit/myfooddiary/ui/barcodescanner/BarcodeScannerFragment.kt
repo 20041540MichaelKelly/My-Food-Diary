1fe097c72ae1cfeca3699942472a173ce51dca06
Michael Kelly <20041540@mail.wit.ie>
2021-12-29 19:35:22 +0000

    scan QR code working now

--- app/src/main/java/org/wit/myfooddiary/ui/barcodescanner/BarcodeScannerFragment.kt
+++ app/src/main/java/org/wit/myfooddiary/ui/barcodescanner/BarcodeScannerFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import com.google.zxing.integration.android.IntentIntegrator
 import com.squareup.picasso.Picasso
 import org.json.JSONException
@@ -14,11 +16,24 @@ import org.json.JSONObject
 import org.wit.myfooddiary.R
 import org.wit.myfooddiary.databinding.FragmentBarcodeScannerBinding
 import org.wit.myfooddiary.helpers.HelperActivity
+import org.wit.myfooddiary.models.FoodModel
+import org.wit.myfooddiary.ui.apifoodlist.ApiFoodListViewModel
+import org.wit.myfooddiary.ui.auth.LoggedInViewModel
+import org.wit.myfooddiary.ui.fooddiary.MyFoodDiaryViewModel
+import org.wit.myfooddiary.ui.foodlist.MyFoodListViewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class BarcodeScannerFragment : Fragment() {
 
     private lateinit var fragBinding: FragmentBarcodeScannerBinding
     private lateinit var qrScanIntegrator: IntentIntegrator
+     var foodItem = FoodModel()
+
+    lateinit var myFoodDiaryViewModel: MyFoodDiaryViewModel
+
+    private val loggedInViewModel: LoggedInViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +41,9 @@ class BarcodeScannerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         fragBinding = FragmentBarcodeScannerBinding.inflate(layoutInflater)
+        myFoodDiaryViewModel = ViewModelProvider(this).get(MyFoodDiaryViewModel::class.java)
+        fragBinding.addBtn.setVisibility(View.GONE);
+
         return fragBinding.root
     }
 
@@ -49,6 +67,11 @@ class BarcodeScannerFragment : Fragment() {
         fragBinding.showQRScanner.setOnClickListener {
             // Add code to show QR Scanner Code in Fragment.
             startActivity(Intent(context, HelperActivity::class.java))
+        }
+
+        fragBinding.addBtn.setOnClickListener{
+            addToFirebaseDB(foodItem)
+
         }
     }
 
@@ -76,11 +99,17 @@ class BarcodeScannerFragment : Fragment() {
                     fragBinding.title.text = obj.getString("title")
                     fragBinding.description.text = obj.getString("description")
                     fragBinding.amountOfCals.text = obj.getString("amountOfCals")
-//                    val imageString = obj.getString("image")
-//                    Picasso.get()
-//                        .load(imageString)
-//                        .into(fragBinding.image)
+                    val imageString = obj.getString("image")
+                    Picasso.get()
+                        .load(imageString)
+                        .into(fragBinding.image)
 
+                    foodItem.title = obj.getString("title")
+                    foodItem.description= obj.getString("description")
+                    val calIntVal = obj.getString("amountOfCals")      //Gathering the data to be added to my list
+                    foodItem.amountOfCals = calIntVal.toInt()
+                    foodItem.image = obj.getString("image")
+                    fragBinding.addBtn.setVisibility(View.VISIBLE);
                 } catch (e: JSONException) {
                     e.printStackTrace()
 
@@ -91,5 +120,26 @@ class BarcodeScannerFragment : Fragment() {
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    private fun addToFirebaseDB(foodItem: FoodModel) {
+        myFoodDiaryViewModel.addFoodItem(
+            loggedInViewModel.liveFirebaseUser,
+            FoodModel(
+                title = foodItem.title,
+                description = foodItem.description,
+                amountOfCals = foodItem.amountOfCals,
+                dateLogged = LocalDateTime.now()
+                    .format(DateTimeFormatter.ofPattern("M/d/y H:m:ss")),
+                timeForFood = System.currentTimeMillis().toString(),
+                image = foodItem.image,
+                email = loggedInViewModel.liveFirebaseUser.value?.email!!,
+                lat = foodItem.lat,
+                lng = foodItem.lng,
+                zoom = 15f
+            )
+        )
+        Toast.makeText(context,"Food Item added",Toast.LENGTH_LONG).show()
+
     }
 }
