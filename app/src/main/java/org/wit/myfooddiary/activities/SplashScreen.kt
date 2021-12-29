53757327ce4e9bd5bdabb8aa5e6f7be91065b768
Michael Kelly <20041540@mail.wit.ie>
2021-12-29 11:19:08 +0000

    switch branch

--- app/src/main/java/org/wit/myfooddiary/activities/SplashScreen.kt
+++ app/src/main/java/org/wit/myfooddiary/activities/SplashScreen.kt
@@ -12,7 +12,7 @@
 //class SplashScreen : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_splash_screen)
+//        setContentView(R.layout.acWillmulti
 //
 //        // This is used to hide the status bar and make
 //        // the splash screen as a full screen activity.
---
+++ app/src/main/java/org/wit/myfooddiary/ui/splash/SplashViewModel.kt
@@ -0,0 +1,9 @@
+package org.wit.myfooddiary.ui.splash
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel: ViewModel() {
+
+
+}
